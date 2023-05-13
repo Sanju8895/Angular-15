76b42fefe7f752b659fdb07b92178994250fde84
Sanjaya Nayak <sanjayanayak67@gmail.com>
2023-05-13 16:53:52 +0530

    custom components added

--- proj1/src/app/app.module.ts
+++ proj1/src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { CustomComponentComponent } from './custom-component/custom-component.component';
 
 @NgModule({
-  declarations: [
+  declarations: [  // components,Directive, Pipes
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    CustomComponentComponent
   ],
-  imports: [
+  imports: [  // module are being imported here
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [],  // all services sre being added here
   bootstrap: [AppComponent]
 })
 export class AppModule { }
