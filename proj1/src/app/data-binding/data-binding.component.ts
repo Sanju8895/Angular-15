4b1129ebcf3ec262fca9856f4249d142a1b0a885
Sanjaya Nayak <sanjayanayak67@gmail.com>
2023-05-14 14:41:41 +0530

    tada typed are being added in ts file

--- proj1/src/app/data-binding/data-binding.component.ts
+++ proj1/src/app/data-binding/data-binding.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class DataBindingComponent {
 
+
+  // String type
+myName:string ="Sanjaya Nayak";
+MyName2:string= "Sanju";
+
+// Number type
+num:number=101;
+num1:number=102.00;
+
+// undefined type
+nonValue:undefined;
+
+// null type
+currentvalue:null=null;
+
+// Boolean Type
+isIndian:boolean= true;
+
+// Array Type
+arr:any[]=[123,"Sanju",true];
+arr1:number[]= [101,102,103,104,105];
+arr2:string[]=["sanju","Biju","Raju"];
+arr3:object[] =[
+  {name:"sanju",age:25,add:"odisha"},
+  {name:"raju",age:27,add:"karnataka"},
+  {name:"pupun",age:22,add:"HYD"}
+];
+
+// Object type
+obj:object= {name:"Sachin" ,age: 30 ,add: "pune"};
+obj1:object ={fname: "Prakash", lname:"Nayak", fullName: function(){return `this.fname this.lname`}};
+
+// Function Syntax
+ myFunc(){
+  let fName ="sanjaya";
+  let lName ="Nayak";
+  return fName+ lName;
+ }
+
 }
